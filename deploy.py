232e2e40c34913176ea8027af2c79a915a5ed7f1
ω'_f <wangfang@o-f.local>
2018-02-02 16:19:53 -0800

    tweak

--- deploy.py
+++ deploy.py
@@ -3,19 +3,21 @@ import paramiko
 
 
 def deploy(key_path, server_address, prefx):
-	print "Connecting to box"
+	print("Connecting to box")
 	ssh = paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	ssh.connect(server_address, username = 'ec2-user', key_filename = key_path)
 	stdin, stdout, stderr = ssh.exec_command('whoami')
-	print stdout.readlines()
+	print(stdout.readlines())
 
 	# git clone the git respository
+	ssh.exec_command('rm -rf bstrat')
 	ssh.exec_command('git clone https://github.com/fwang18/bstrat.git')
+	ssh.exec_command('cd bstrat/; ls')
 
 
 	
 
 
 if __name__ == "__main__":
-	deploy('/Users/chengcheng/credentials/bstrat.pem', 'ec2-34-211-189-8.us-west-2.compute.amazonaws.com', 'user')
+	deploy('/Users/wangfang/aws/bstrat.pem', 'ec2-34-211-189-8.us-west-2.compute.amazonaws.com', 'user')
