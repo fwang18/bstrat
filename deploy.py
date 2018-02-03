ed87350e4e89118174135cd100ecfe49212f6109
ω'_f <wangfang@o-f.local>
2018-02-02 16:29:30 -0800

    0202_4pm

--- deploy.py
+++ deploy.py
@@ -8,15 +8,16 @@ def deploy(key_path, server_address, prefx):
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	ssh.connect(server_address, username = 'ec2-user', key_filename = key_path)
 	stdin, stdout, stderr = ssh.exec_command('whoami')
-	print(stdout.readlines())
+	print('connected as', stdout.readlines()[0])
 
 	# git clone the git respository
-	ssh.exec_command('rm -rf bstrat')
+	ssh.exec_command('rm -rf bstrat') #remove old ones so we can write new repo
 	ssh.exec_command('git clone https://github.com/fwang18/bstrat.git')
-	ssh.exec_command('cd bstrat/; ls')
+	ssh.exec_command('cd bstrat/')
+	ssh.exec_command('python json_process.py > log.txt') #not working???
 
 
-	
+
 
 
 if __name__ == "__main__":
