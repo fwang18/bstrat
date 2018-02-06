4f1ba06a2d6e8e775a8b3f8eeb4ba4595118af61
ω'_f <wangfang@o-f.local>
2018-02-05 21:03:09 -0800

    0205

--- deploy.py
+++ deploy.py
@@ -6,7 +6,7 @@ def deploy(key_path, server_address, prefx):
 	print("Connecting to box")
 	ssh = paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	ssh.connect(server_address, username = 'ec2-user', key_filename = key_path)
+	ssh.connect(server_address, username = 'testtest', key_filename = key_path)
 	stdin, stdout, stderr = ssh.exec_command('whoami')
 	print('connected as', stdout.readlines()[0])
 
@@ -14,7 +14,7 @@ def deploy(key_path, server_address, prefx):
 	ssh.exec_command('rm -rf bstrat') #remove old ones so we can write new repo
 	ssh.exec_command('git clone https://github.com/fwang18/bstrat.git')
 	ssh.exec_command('cd bstrat/')
-	ssh.exec_command('python json_process.py > log.txt') #not working???
+	ssh.exec_command('(crontab -l ; echo "*/5 * * * * sudo python /home/testtest/bstrat/json_process.py %s") | crontab -' % prefix) #this line is not testted
 
 
 
