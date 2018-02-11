b10c07f2d9805ef9af0477d133e56b7cc8ca9072
ω'_f <wangfang@o-f.local>
2018-02-10 17:02:06 -0800

    0210

--- deploy.py
+++ deploy.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python
 import paramiko
+import sys
 
+prefix=sys.argv[1]
 
 def deploy(key_path, server_address, prefix):
-	print("Connecting to box")
 	ssh = paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	ssh.connect(server_address, username = 'testtest', key_filename = key_path)
+	ssh.connect(server_address, username = 'testtest', key_filename = key_path) #can use 'ec2-user' to replce 'testtest' for test
 	stdin, stdout, stderr = ssh.exec_command('whoami')
-	print('connected as', stdout.readlines()[0])
+	print("#####################################")
+	print("####                             ####")
+	print("####    Connected to ec2 box     ####")
+	print("####                             ####")
+	print("#####################################")
+	print('User name: ', stdout.readlines()[0])
 
 	# git clone the git respository
 	ssh.exec_command('rm -rf bstrat') #remove old ones so we can write new repo
 	ssh.exec_command('git clone https://github.com/fwang18/bstrat.git')
 	ssh.exec_command('cd bstrat/')
-	ssh.exec_command('crontab -r')
-	ssh.exec_command('(crontab -l ; echo "*/5 * * * * sudo python /home/testtest/bstrat/json_process.py %s") | crontab -' % prefix) #this line is not testted
+	#ssh.exec_command('crontab -r') #might cause a problem when multiple tests are conducted at the same time
+
+	ssh.exec_command('(crontab -l ; echo "*/5 * * * * python /home/testtest/bstrat/process_json.py /srv/runme %s") | crontab -' % prefix)
+	#to test, change '*/5 * * * *' into '*/1 * * * *'
+
+	print("process finished")
 
 
-
-
-
+#### running example, commented when submitted, uncomment for test use
+'''
 if __name__ == "__main__":
-	deploy('/Users/wangfang/aws/bstrat.pem', 'ec2-34-211-189-8.us-west-2.compute.amazonaws.com', 'user')
+	deploy('/Users/wangfang/aws/bstrat.pem', 'ec2-34-211-189-8.us-west-2.compute.amazonaws.com', prefix)
+'''
