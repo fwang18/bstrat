2308c9b9493c937cfcf9ec33173cde73a22624bd
chengchengx <cxu29@usfca.edu>
2018-02-11 14:43:57 -0800

    add crobtab -r

--- deploy.py
+++ deploy.py
@@ -2,7 +2,6 @@
 import paramiko
 import sys
 
-prefix=sys.argv[1]
 
 def deploy(key_path, server_address, prefix):
 	ssh = paramiko.SSHClient()
@@ -18,10 +17,12 @@ def deploy(key_path, server_address, prefix):
 
 	# git clone the git respository
 	ssh.exec_command('rm -rf bstrat') #remove old ones so we can write new repo
+
 	ssh.exec_command('git clone https://github.com/fwang18/bstrat.git')
 	ssh.exec_command('cd bstrat/')
 	#ssh.exec_command('crontab -r') #might cause a problem when multiple tests are conducted at the same time
 
+	ssh.exec_command('crontab -r')
 	ssh.exec_command('(crontab -l ; echo "*/5 * * * * python /home/testtest/bstrat/process_json.py /srv/runme %s") | crontab -' % prefix)
 	#to test, change '*/5 * * * *' into '*/1 * * * *'
 
@@ -29,7 +30,6 @@ def deploy(key_path, server_address, prefix):
 
 
 #### running example, commented when submitted, uncomment for test use
-'''
+
 if __name__ == "__main__":
-	deploy('/Users/wangfang/aws/bstrat.pem', 'ec2-34-211-189-8.us-west-2.compute.amazonaws.com', prefix)
-'''
+	deploy('/Users/chengcheng/credentials/bstrat.pem', 'ec2-34-211-189-8.us-west-2.compute.amazonaws.com', "sample")
