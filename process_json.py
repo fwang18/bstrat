78ed8f4555078bc2fd8cacc567983d01be5c31e4
Jingjue Wang <jingjuewang@Jingjues-MacBook-Pro.local>
2018-02-11 14:54:05 -0800

    print filepath

--- process_json.py
+++ process_json.py
@@ -23,7 +23,7 @@ def fetch_elements(filepath):
                                     
                                         if age > 0:
                                             if len(name)!=0:
-                                                output_for_one = output_for_one + name + '\t' + str(age) + '\n'
+                                                output_for_one = output_for_one + name + '\t' + str(age) + filepath+'\n'
 				except KeyError:
 					continue
 			except ValueError:
@@ -39,7 +39,7 @@ all_output = ''
 for f in filepaths:
 	output_for_one = fetch_elements(f)
         if len(output_for_one) > 0:
-            all_output = all_output + '\n' + output_for_one + f
+            all_output = all_output + '\n' + output_for_one
 
 
 write_to = mypath +"/"+ myprefix + '.txt'
