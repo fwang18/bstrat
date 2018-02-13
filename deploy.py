50d2a8b3a041c2e49662f9bac9279c6f6d080503
chengchengx <cxu29@usfca.edu>
2018-02-12 22:07:13 -0800

    remove main function

--- deploy.py
+++ deploy.py
@@ -33,5 +33,5 @@ def deploy(key_path, server_address, prefix):
 
 #### running example, commented when submitted, uncomment for test use
 
-if __name__ == "__main__":
-	deploy('/Users/chengcheng/credentials/bstrat.pem', 'ec2-34-211-189-8.us-west-2.compute.amazonaws.com', "sample")
+# if __name__ == "__main__":
+# 	deploy('/Users/chengcheng/credentials/bstrat.pem', 'ec2-34-211-189-8.us-west-2.compute.amazonaws.com', "sample")
