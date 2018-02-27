d585d5504ebce3adc93e885847034fee853caa77
chengchengx <cxu29@usfca.edu>
2018-02-27 06:52:19 +0000

    update set_flask.py

--- set_flask.py
+++ set_flask.py
@@ -1,27 +1,26 @@
 from flask import Flask
 from process_json import *
+from flask import request
+import sys
+
 app = Flask(__name__)
+prefix = ""
 
 def add_line(f_name, content):
     with open(f_name, 'a') as file:
         file.write(content+'\n')
 
-#print(process_one("{\"name\": \"Nick\",\"prop\":{\"age\": 32, \"zipcode\": 94607, \"DMID\": 388167}}"))
+#print(process_one("{\"name\": \"Nick\",\"prop\":{\"age\": 32, \"zipcode\": 94607, \"DMID\": 388167}}")) 
 
-@app.route('/')  
-def hello_world():  
-    add_line('root.txt', 'hello')
-    return 'Hello World!'  
-
-@app.route('/application/<json_b>', methods = ["POST"])
-def process_json(json_b):
-    add_line('Raw.txt', json_b)
-    a=process_one(json_b)
-    if a is not None:
-        add_line('proc.txt', a)
-    return a
- 
-
+@app.route('/application', methods = ['POST'])
+def process_json():    
+    add_line('/srv/runme/' + prefix + '/Raw.txt', request.data)
+    result = process_one(request.data)
+    if result is not None and len(result) > 0:
+        add_line('/srv/runme/' + prefix + '/proc.txt', result)
+        # add_line('/srv/runme/proc.txt', result)
+    return result
 
 if __name__ == '__main__':
+    prefix = sys.argv[2]
     app.run('0.0.0.0', port=8080)
