c2ff149e44fcabab6c8a47813f1603a99a645822
chengchengx <cxu29@usfca.edu>
2018-02-27 06:52:49 +0000

    update log ratations

---
+++ log_proc.py
@@ -0,0 +1,37 @@
+#import logging
+# from logging.handlers import RotatingFileHandler
+#import logging, logging.handlers
+
+#get the root logger
+#logger = logging.getLogger("Rotating Log")
+#set overall level to debug, default is warning for root logger
+#logger.setLevel(logging.INFO)
+#handler = logging.handlers.TimedRotatingFileHandler('/srv/runme/sample/Raw.txt',when='M',interval=1, backupCount=10)
+#filelog.setLevel(logging.DEBUG)
+
+import logging
+import time
+ 
+from logging.handlers import TimedRotatingFileHandler
+ 
+#----------------------------------------------------------------------
+def create_timed_rotating_log(path):
+    """"""
+    logger = logging.getLogger("Rotating proc.txt ")
+    logger.setLevel(logging.INFO)
+ 
+    handler = TimedRotatingFileHandler(path,
+                                       when="m",
+                                       interval=1,
+                                       backupCount=100)
+    logger.addHandler(handler)
+ 
+    for i in range(6):
+        logger.info("This is a test!")
+        time.sleep(75)
+
+#----------------------------------------------------------------------
+if __name__ == "__main__":
+    log_file = "/srv/runme/" + sys.argv[2] + "/proc.txt"
+    # log_file = "/srv/runme/sample/proc.txt"
+    create_timed_rotating_log(log_file)
